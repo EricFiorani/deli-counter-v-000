4b4efd233baeadc25406bcb2d420b39fa4f148da
Eric Fiorani <efiorani305@gmail.com>
2018-04-15 00:30:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,9 +14,9 @@ end
 
 def now_serving(other_deli)
   if other_deli != []
-      puts "Currently serving #{other_deli[0]}."
-      other_deli.shift
+    puts "Currently serving #{other_deli[0]}."
+    other_deli.shift
   else
-      puts"There is nobody waiting to be served!"
-    end
+    puts"There is nobody waiting to be served!"
+  end
 end
