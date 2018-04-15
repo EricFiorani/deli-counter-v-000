735bb2e47f3da71c29da90653bb398574d1bd1c1
Eric Fiorani <efiorani305@gmail.com>
2018-04-15 00:15:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli == []
+  if katz_deli == 0
     puts "The line is currently empty."
   elsif katz_deli == []
 
@@ -12,3 +12,8 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "The line is currently empty."
+  
