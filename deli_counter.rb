a7fbd52655217e9abb40fda5eae96d1a6b0002a0
Eric Fiorani <efiorani305@gmail.com>
2018-04-15 00:30:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(other_deli)
-  if other_deli.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
+  if other_deli != []
+      puts "Currently serving #{other_deli[0]}."
+      other_deli.shift
+    else
+      puts"There is nobody waiting to be served!"
+    end
 end
